0d0909ec2e51857f2fc5187c604a0ed23306b47e
igjit <igjit1@gmail.com>
2018-11-08 07:49:52 +0900

    Test paren

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -5,4 +5,5 @@ test_that("parse", {
     expect_equal(parse(tokenize("1+2")), node("+", node_num(1), node_num(2)))
     expect_equal(parse(tokenize("1+2*3")), node("+", node_num(1), node("*", node_num(2), node_num(3))))
     expect_equal(parse(tokenize("1*2+3")), node("+", node("*", node_num(1), node_num(2)), node_num(3)))
+    expect_equal(parse(tokenize("1*(2+3)")), node("*", node_num(1), node("+", node_num(2), node_num(3))))
 })
