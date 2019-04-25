3f3d5069822969565f08ec7f0d1d32af581409d3
igjit <igjit1@gmail.com>
2019-04-26 04:13:23 +0900

    :golf:

--- tests/testthat/test-generator.R
+++ tests/testthat/test-generator.R
@@ -3,5 +3,5 @@ context("generator.R")
 test_that("print.assembly", {
     asm <- c(".intel_syntax noprefix", ".global main")
     class(asm) <- "assembly"
-    expect_equal(capture_output(print(asm)), paste(asm, collapse = "\n"))
+    expect_output(print(asm), paste(asm, collapse = "\n"))
 })
