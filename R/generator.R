37148c7104fba60e30b83ede7dcfaa7300351d78
igjit <igjit1@gmail.com>
2019-12-12 20:01:40 +0900

    Set indent level to 2

--- R/generator.R
+++ R/generator.R
@@ -4,80 +4,80 @@
 #' @return assembly
 #' @export
 generate <- function(nodes) {
-    c(body, n_var) %<-% generate_body(nodes)
-    l <- c(".intel_syntax noprefix",
-           ".global main",
-           "main:",
-           indent("push rbp",
-                  "mov rbp, rsp",
-                  paste0("sub rsp, ", n_var * 8)),
-           indent(body),
-           indent("mov rsp, rbp",
-                  "pop rbp",
-                  "ret"))
-    structure(l, class = "assembly")
+  c(body, n_var) %<-% generate_body(nodes)
+  l <- c(".intel_syntax noprefix",
+         ".global main",
+         "main:",
+         indent("push rbp",
+                "mov rbp, rsp",
+                paste0("sub rsp, ", n_var * 8)),
+         indent(body),
+         indent("mov rsp, rbp",
+                "pop rbp",
+                "ret"))
+  structure(l, class = "assembly")
 }
 
 #' @export
 print.assembly <- function (x, ...) {
-    cat(x, sep = "\n")
-    invisible(x)
+  cat(x, sep = "\n")
+  invisible(x)
 }
 
 generate_body <- function(nodes) {
-    vars <- var_map()
-    body <- nodes %>%
-        map(generate_node, vars) %>%
-        map2("pop rax", c) %>%
-        flatten_chr
-    list(body, length(vars))
+  vars <- var_map()
+  body <- nodes %>%
+    map(generate_node, vars) %>%
+    map2("pop rax", c) %>%
+    flatten_chr
+  list(body, length(vars))
 }
 
 generate_node <- function(node, vars) {
-    if (is_num(node)) {
-        paste0("push ", val(node))
-    } else if (is_ident(node)) {
-        c(generate_lvalue(node, vars),
-          "pop rax",
-          "mov rax, [rax]",
-          "push rax")
-    } else if (node$op == "=") {
-        c(generate_lvalue(node$lhs, vars),
-          generate_node(node$rhs, vars),
-          "pop rdi",
-          "pop rax",
-          "mov [rax], rdi",
-          "push rdi")
-    } else {
-        c(generate_node(node$lhs, vars),
-          generate_node(node$rhs, vars),
-          "pop rdi",
-          "pop rax",
-          switch(node$op,
-                 "+" = "add rax, rdi",
-                 "-" = "sub rax, rdi",
-                 "*" = "mul rdi",
-                 "/" = c("mov rdx, 0",
-                         "div rdi"),
-                 "==" = c("cmp rdi, rax",
-                          "sete al",
-                          "movzb rax, al"),
-                 "!=" = c("cmp rdi, rax",
-                          "setne al",
-                          "movzb rax, al")),
-          "push rax")
-    }
+  if (is_num(node)) {
+    paste0("push ", val(node))
+  } else if (is_ident(node)) {
+    c(generate_lvalue(node, vars),
+      "pop rax",
+      "mov rax, [rax]",
+      "push rax")
+  } else if (node$op == "=") {
+    c(generate_lvalue(node$lhs, vars),
+      generate_node(node$rhs, vars),
+      "pop rdi",
+      "pop rax",
+      "mov [rax], rdi",
+      "push rdi")
+  } else {
+    c(generate_node(node$lhs, vars),
+      generate_node(node$rhs, vars),
+      "pop rdi",
+      "pop rax",
+      switch(node$op,
+             "+" = "add rax, rdi",
+             "-" = "sub rax, rdi",
+             "*" = "mul rdi",
+             "/" = c("mov rdx, 0",
+                     "div rdi"),
+             "==" = c("cmp rdi, rax",
+                      "sete al",
+                      "movzb rax, al"),
+             "!=" = c("cmp rdi, rax",
+                      "setne al",
+                      "movzb rax, al")),
+      "push rax")
+  }
 }
 
 generate_lvalue <- function(node, vars) {
-    if (is_ident(node)) {
-        adr <- index_of(val(node), vars) * 8
-        c("mov rax, rbp",
-          paste0("sub rax, ", adr),
-          "push rax")
-    } else {
-        stop("invalid lvalue")
-    }
+  if (is_ident(node)) {
+    adr <- index_of(val(node), vars) * 8
+    c("mov rax, rbp",
+      paste0("sub rax, ", adr),
+      "push rax")
+  } else {
+    stop("invalid lvalue")
+  }
 }
 
 indent <- function(...) paste0("  ", c(...))
@@ -85,9 +85,9 @@ indent <- function(...) paste0("  ", c(...))
 var_map <- function() new.env(parent = emptyenv())
 
 index_of <- function(var, var_map) {
-    if (is.null(var_map[[var]])) {
-        index <- length(var_map) + 1L
-        var_map[[var]] <- index
-    }
-    var_map[[var]]
+  if (is.null(var_map[[var]])) {
+    index <- length(var_map) + 1L
+    var_map[[var]] <- index
+  }
+  var_map[[var]]
 }
--- tests/testthat/test-generator.R
+++ tests/testthat/test-generator.R
@@ -1,13 +1,13 @@
 context("generator.R")
 
 test_that("print.assembly", {
-    asm <- c(".intel_syntax noprefix", ".global main")
-    class(asm) <- "assembly"
-    expect_output(print(asm), paste(asm, collapse = "\n"))
+  asm <- c(".intel_syntax noprefix", ".global main")
+  class(asm) <- "assembly"
+  expect_output(print(asm), paste(asm, collapse = "\n"))
 })
 
 test_that("index_of", {
-    vars <- var_map()
-    indices <- map_int(c("a", "b", "c", "b"), ~ index_of(., vars))
-    expect_equal(indices, c(1, 2, 3, 2))
+  vars <- var_map()
+  indices <- map_int(c("a", "b", "c", "b"), ~ index_of(., vars))
+  expect_equal(indices, c(1, 2, 3, 2))
 })
