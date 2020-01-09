3cbcbce9b031dea4752aaf34148ab330fc12a3ab
igjit <igjit1@gmail.com>
2020-01-09 19:01:08 +0900

    :golf:

--- R/converter.R
+++ R/converter.R
@@ -2,17 +2,15 @@ convert <- function(nodes) {
   extract_functions(nodes)
 }
 
-extract_functions <- function(nodes, index = 1, acm = list(functions = list(), nodes = list())) {
-  if (length(nodes) < index) {
-    acm
-  } else {
-    node <- nodes[[index]]
+extract_functions <- function(nodes) {
+  reduce(nodes, function(acm, node) {
     if (node$op == "=" && is(node$rhs, "node_function")) {
       name <- val(node$lhs)
       acm$functions[[name]] <- node$rhs
     } else {
       acm$nodes <- c(acm$nodes, list(node))
     }
-    extract_functions(nodes, index + 1, acm)
-  }
+    acm
+  },
+  .init = list(functions = list(), nodes = list()))
 }
